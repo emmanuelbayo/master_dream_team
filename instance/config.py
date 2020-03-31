7036fa97ffee72827084a881ce07f81ec18fa06f
emmanuelbayo <bayopatrick2@gmail.com>
2020-03-31 12:37:26 +0000

    new update commit

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,3 @@
 SECRET_KEY= '9bc4752d4f5dd7beae3a0011bce4b7e0'
 
-DATABASE_URLc = 'mysql://dt_admin:nono012369@localhost/dreamteam_db'
+DATABASE_URL = 'mysql://dt_admin:nono012369@localhost/dreamteam_db'
