3580a2a4f2a3022926fd124f06ccad53342025e5
emmanuelbayo <bayopatrick2@gmail.com>
2020-04-02 15:09:45 +0000

    new update

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,5 @@
+import os
+
 SECRET_KEY= '9bc4752d4f5dd7beae3a0011bce4b7e0'
 
-DATABASE_URL = 'mysql://dt_admin:nono012369@localhost/dreamteam_db'
+SQLALCHEMY_DATABASE_URI = 'postgres://byjvpfgxqagjvk:62ff9ae518a003e978ca483b5db4ed7f9a1f1d5223fecf43ee45256c18c4cb97@ec2-34-193-232-231.compute-1.amazonaws.com:5432/df1flvt0o23vp2'
